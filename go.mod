597329b2943421b213c19de865fbf7334a682c01
Hnasir112 <hamzanasir9299@gmail.com>
2026-01-12 15:47:06 +0300

    testing go version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Hnasir112/my-git
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func runCommand(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func saveChanges(message string) {
+	fmt.Println("Adding files...")
+	if err := runCommand("git", "add", "."); err != nil {
+		fmt.Printf("Error adding files: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Commiting with message: %s\n", message)
+	if err := runCommand("git", "commit", "-m", message); err != nil {
+		fmt.Printf("Error committing changes: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Pushing to remote...")
+	if err := runCommand("git", "push"); err != nil {
+		fmt.Printf("Error pushing changes: %v\n", err)
+		return
+	}
+
+	fmt.Println("Done!")
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: mygit save \"commit message\"")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	if command == "save" {
+		message := os.Args[2]
+		saveChanges(message)
+	} else {
+		fmt.Printf("Unknown command: %s\n", command)
+		os.Exit(1)
+	}
+}
